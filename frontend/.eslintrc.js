ef80972c45026af336482ed0b2ab034c0a5d00af
fannypag <81959515+fannypag@users.noreply.github.com>
2022-11-16 13:12:09 +0700

    .

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -6,7 +6,6 @@ module.exports = {
   extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    'plugin:prettier/recommended',
   ],
   parserOptions: {
     parser: '@babel/eslint-parser',
@@ -15,6 +14,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'vue/multi-word-component-names': 'off',
+    'prettier/prettier': 0,
   },
   overrides: [
     {
--- frontend/src/views/pages/Login.vue
+++ frontend/src/views/pages/Login.vue
@@ -29,31 +29,16 @@
                     />
                   </CInputGroup>
                   <CRow>
-                    <CCol :xs="6">
-                      <CButton color="primary" class="px-4"> Login </CButton>
+                    <CCol>
+                      <CButton color="primary" class="px-4"> 
+                        Login </CButton>
                     </CCol>
-                    <CCol :xs="6" class="text-right">
-                      <CButton color="link" class="px-0">
-                        Forgot password?
-                      </CButton>
+                    <CCol>
+                      <CButton color="primary" class="px-4">
+                        Create account</CButton>
                     </CCol>
                   </CRow>
                 </CForm>
-              </CCardBody>
-            </CCard>
-            <CCard class="text-white bg-primary py-5" style="width: 44%">
-              <CCardBody class="text-center">
-                <div>
-                  <h2>Sign up</h2>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                    sed do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua.
-                  </p>
-                  <CButton color="light" variant="outline" class="mt-3">
-                    Register Now!
-                  </CButton>
-                </div>
               </CCardBody>
             </CCard>
           </CCardGroup>
