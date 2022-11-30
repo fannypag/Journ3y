086aaa90f42c74c45e1968e0bb0b55399d246647
rifatiyay <salmarifatia@gmail.com>
2022-11-30 09:40:53 +0700

    .

--- frontend/src/components/AppSidebar.vue
+++ frontend/src/components/AppSidebar.vue
@@ -12,16 +12,7 @@
     "
   >
     <CSidebarBrand>
-      <CIcon
-        custom-class-name="sidebar-brand-full"
-        :icon="logoNegative"
-        :height="35"
-      />
-      <CIcon
-        custom-class-name="sidebar-brand-narrow"
-        :icon="sygnet"
-        :height="35"
-      />
+      <CAvatar :src="logoJourney" />
     </CSidebarBrand>
     <AppSidebarNav />
     <CSidebarToggler
@@ -37,16 +28,24 @@ import { useStore } from 'vuex'
 import { AppSidebarNav } from './AppSidebarNav'
 import { logoNegative } from '@/assets/brand/logo-negative'
 import { sygnet } from '@/assets/brand/sygnet'
+import logoJourney from "@/assets/images/journey.png"
+// import CIcon from '@coreui/icons-vue'
+// import { CAvatarPlugin } from '@coreui/vue'
+// import { CImage } from '@coreui/vue'
+
 export default {
   name: 'AppSidebar',
   components: {
     AppSidebarNav,
-  },
+    // CIcon,
+    // CAvatarPlugin
+},
   setup() {
     const store = useStore()
     return {
       logoNegative,
       sygnet,
+      logoJourney,
       sidebarUnfoldable: computed(() => store.state.sidebarUnfoldable),
       sidebarVisible: computed(() => store.state.sidebarVisible),
     }
